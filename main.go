86fa31a8238645971f0ac3162a091bf029a64692
hello <keye.myria@gmail.com>
2023-02-10 16:10:11 +0800

    version

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "omgind-cli"
 	app.Description = "omgind 辅助工具，提供创建项目、快速生成功能模块的功能"
-	app.Version = "1.0.1"
+	app.Version = "1.0.3"
 	app.Commands = []cli.Command{
 		cmd.NewCommand(),
 		cmd.GenerateCommand(),
