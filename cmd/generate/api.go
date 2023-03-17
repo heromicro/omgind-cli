c830723ee5e9b1c32d22f47b13a4dd738c0a42e4
hello <keye.myria@gmail.com>
2023-03-17 17:42:44 +0800

    update api error

--- cmd/generate/api.go
+++ cmd/generate/api.go
@@ -89,6 +89,17 @@ func (a *{{.Name}}) Get(c *gin.Context) {
 	ginx.ResSuccess(c, item)
 }
 
+// View 查询指定数据
+func (a *{{.Name}}) View(c *gin.Context) {
+	ctx := c.Request.Context()
+	item, err := a.{{.Name}}Srv.View(ctx, c.Param("id"))
+	if err != nil {
+		ginx.ResError(c, err)
+		return
+	}
+	ginx.ResSuccess(c, item)
+}
+
 // Create 创建数据
 func (a *{{.Name}}) Create(c *gin.Context) {
 	ctx := c.Request.Context()
